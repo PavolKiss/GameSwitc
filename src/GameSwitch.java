e591e3a27160ad89b2f6077cb8c49d26e0a026f2
Pavol Kiss <pavol.kiss@akademiasovy.com>
2018-04-05 13:59:10 +0200

    init

--- src/GameSwitch.java
+++ src/GameSwitch.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameSwitch {
     private JPanel GamePanel;
@@ -20,6 +22,139 @@ public class GameSwitch {
     private JButton button16;
     private JButton button17;
     private JButton button18;
+    private State state = State.NEW;
+    private boolean[][] field  = new boolean[4][4];
+
+    public GameSwitch() {
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(5);
+            }
+        });
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(3);
+            }
+        });
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(4);
+            }
+        });
+        button6.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(6);
+            }
+        });
+        button7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(7);
+            }
+        });
+        button10.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(10);
+            }
+        });
+        button11.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(11);
+            }
+        });
+        button12.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(12);
+            }
+        });
+        button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(8);
+            }
+        });
+        button15.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(15);
+            }
+        });
+        button14.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(14);
+            }
+        });
+        button13.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(13);
+            }
+        });
+        button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(9);
+            }
+        });
+        button18.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(18);
+            }
+        });
+        button17.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(17);
+            }
+        });
+        button16.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                processClick(16);
+            }
+        });
+        exitGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        newGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                state = State.PLAYING;
+                for (int i = 0; i<4;i++){
+                    for (int j=0; j<3; j++){
+                        field[i][j] = false;
+                    }
+                }
+            }
+        });
+    }
+
+    private void processClick(int i){
+        if (state == State.PLAYING)
+        System.out.println("You clicked on button " +i);
+    }
+
+    public void toggle(int i){
+        int col = (i-1)%4;
+        int row =  (i-1)/4;
+        field [row][col] = !field[row][col];
+        if (field[row][col] == false){
+
+        }
+    }
 
     public JPanel getGamePanel() {
         return GamePanel;
